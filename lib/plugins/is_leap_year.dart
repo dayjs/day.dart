49476b003d0a627f42d8dd907614ef52b5b1d5cb
Yue Yang <g1enyy0ung@gmail.com>
2019-12-13 17:01:52 +0800

    Update is_leap_year.dart

--- lib/plugins/is_leap_year.dart
+++ lib/plugins/is_leap_year.dart
@@ -1,6 +1,8 @@
-import '../src/day.dart';
+import '../day.dart';
 
-// https://stackoverflow.com/a/11595914/5676489
+/// Check a [Day] instance is a leap year or not.
+///
+/// Reference: https://stackoverflow.com/a/11595914/5676489
 extension IsLeapYear on Day {
   bool isLeapYear() {
     final y = this.year();
