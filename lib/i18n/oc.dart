d99f43713f3a3af333faaa19258c271076fb2475
Mejans <61360811+Mejans@users.noreply.github.com>
2026-09-21 09:26:02 +0200

    i18n: add Occitan locale file (#46)

---
+++ lib/i18n/oc.dart
@@ -0,0 +1,43 @@
+final Map<String, dynamic> locale = {
+  'Name': 'oc',
+  'Months': {
+    1: 'Genièr',
+    2: 'Febrièr',
+    3: 'Març',
+    4: 'Abril',
+    5: 'Mai',
+    6: 'Junh',
+    7: 'Julhet',
+    8: 'Agost',
+    9: 'Setembre',
+    10: 'Octòbre',
+    11: 'Novembre',
+    12: 'Decembre'
+  },
+  'Weekdays': {
+    1: 'Diluns',
+    2: 'Dimars',
+    3: 'Dimècres',
+    4: 'Dijòus',
+    5: 'Divendres',
+    6: 'Dissabte',
+    7: 'Dimenge'
+  },
+  'AM': 'AM',
+  'PM': 'PM',
+  'RelativeTime': {
+    'future': 'dins %s',
+    'past': 'fa %s',
+    's': 'unas segondas',
+    'm': 'una minuta',
+    'mm': '%d minutas',
+    'h': 'un ora',
+    'hh': '%d oras',
+    'd': 'un jorn',
+    'dd': '%d jorns',
+    'M': 'un mes',
+    'MM': '%d meses',
+    'y': 'un an',
+    'yy': '%d ans'
+  }
+};
